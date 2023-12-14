114c6319b1c3de712620b8810cc9e897a3a8e41e
Ruslan_BRV <ruslan10897@mail.ru>
2023-12-14 16:30:38 +0300

    Добавил отображение картинок продуктов и страницу админа

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 from db.models import *
 
 app = Flask(__name__)
@@ -19,6 +19,21 @@ def productsPage():
 @app.route("/contacts")
 def contactsPage():
     return render_template("contacts.html")
+
+@app.route("/admin")
+def adminPage():
+    if "login" not in session:
+        return redirect("/admin/login")
+    
+@app.route("/admin/login", methods=["GET", "POST"])
+def adminLoginPage():
+    if request.method == "GET":
+        return render_template("adminLogin.html")
+    elif request.method == "POST":
+        login = request.form["login"]
+        password = request.form["password"]
+
+        
 
 
 app.run(debug=True)
